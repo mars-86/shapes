c8be5d5abd3387a0c0c7da104e4cd6786bac6bed
mars-86 <hrslash86@gmail.com>
2023-10-08 00:51:34 -0300

    refactor: modify types.h path in linux prototypes file

--- lib/__sys/linux/shapes.h
+++ lib/__sys/linux/shapes.h
@@ -2,34 +2,34 @@
 #define __LIB_SYS_LINUX_SHAPES_INCLUDED_H__
 
 #include <stdio.h>
-#include "../../types.h"
+#include "../types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-extern line_t *__cdecl line(int __x1, int __y1, int __x2, int __y2)
+extern line_t * line(int __x1, int __y1, int __x2, int __y2)
     __attribute__((nothrow));
 
-extern rect_t *__cdecl rect(int __x1, int __y1, size_t __width, size_t __height)
+extern rect_t * rect(int __x1, int __y1, size_t __width, size_t __height)
     __attribute__((nothrow));
 
-extern square_t *__cdecl square(int __x1, int __y1, size_t __size)
+extern square_t * square(int __x1, int __y1, size_t __size)
     __attribute__((nothrow));
 
-extern circle_t *__cdecl circle(int __x1, int __y1, size_t __radius)
+extern circle_t * circle(int __x1, int __y1, size_t __radius)
     __attribute__((nothrow));
 
-extern void __cdecl print_line(line_t *__line)
+extern void print_line(line_t *__line)
     __attribute__((nothrow)) __attribute__((nonnull (1)));
 
-extern void __cdecl print_rect(rect_t *__rect)
+extern void print_rect(rect_t *__rect)
     __attribute__((nothrow)) __attribute__((nonnull (1)));
 
-extern void __cdecl print_square(square_t *__square)
+extern void print_square(square_t *__square)
     __attribute__((nothrow)) __attribute__((nonnull (1)));
 
-extern void __cdecl print_circle(circle_t *__circle)
+extern void print_circle(circle_t *__circle)
     __attribute__((nothrow)) __attribute__((nonnull (1)));
 
 #ifdef __cplusplus
