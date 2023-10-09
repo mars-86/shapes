ff2b5ce7fa611d98bac7a47890732def5b7d6047
mars-86 <hrslash86@gmail.com>
2023-10-09 13:28:27 -0300

    feat: added function to pre-calculate line size

--- main.c
+++ main.c
@@ -4,20 +4,20 @@
 
 int main(int argc, char *argv[])
 {
-    // line_t *l1 = line(-20, 70, 50, 143);
+    line_t *l1 = line(-20, -70, -60, -143);
     // rect_t *r1 = rect(0, 0, 1, 1);
     // square_t *s1 = square(0, 0, 5);
-    circle_t *c1 = circle(0, 0, 100);
+    // circle_t *c1 = circle(0, 0, 100);
 
-    // print_line(l1);
+    print_line(l1);
     // print_rect(r1);
     // print_square(s1);
-    print_circle(c1);
+    // print_circle(c1);
 
-    // free(l1);
+    free(l1);
     // free(r1);
     // free(s1);
-    free(c1);
+    // free(c1);
 
     return 0;
 }
